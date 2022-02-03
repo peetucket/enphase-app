c2049ae02fd7a478c264d8ff505c0c6b9d46b70f
Peter Mangiafico <peter@mangiafico.org>
2022-02-03 14:02:08 -0800

    add the ability to require a token to view the site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :check_authorization
+
+  def check_authorization
+    return if Settings.token.blank? # ok if we have no token
+    head :forbidden unless params[:token] == Settings.token # forbidden unless token matches
+  end
 end
