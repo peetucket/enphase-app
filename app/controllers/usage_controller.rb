a2f5f0e680ae2c2f942ff212c4f1e61e08c87813
Peter Mangiafico <peter@mangiafico.org>
2022-02-07 08:16:03 -0800

    fix inverter api issue

--- app/controllers/usage_controller.rb
+++ app/controllers/usage_controller.rb
@@ -5,7 +5,7 @@ class UsageController < ApplicationController
     inverters = EnphaseClient.conn.inverters(site_id: Settings.enphase.system_id)
     @active_inverters = inverters[0]['micro_inverters'].reject {|inverter| inverter["status"] == 'retired'}
     @inverter_status = @active_inverters.all? {|inverter| inverter['status'] == 'normal'} ? 'normal' : 'alert'
-    @inverter_production = @active_inverters.sum {|inverter| inverter['power_produced']}
+    @inverter_production = @active_inverters.sum {|inverter| inverter['power_produced']['value']}
     @status = @result["status"]
   end
 
